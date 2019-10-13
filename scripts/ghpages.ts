4aab7cd5cd7502c4e5483ee751890bb10edcf173
SatowTakeshi <doublequotation@gmail.com>
2019-10-14 07:03:07 +0900

    request build

--- scripts/ghpages.ts
+++ scripts/ghpages.ts
@@ -1,6 +1,18 @@
 import * as ghpages from 'gh-pages';
+import * as fetch from 'node-fetch';
 
 const { GITHUB_ACTOR, GITHUB_REPOSITORY, GITHUB_TOKEN } = process.env;
+const BUILD_REQ_URL = `https://api.github.com/repos/${GITHUB_REPOSITORY}/pages/builds`;
+
+const opt = {
+    method: 'POST',
+    headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+        Accept: 'application/vnd.github.v3+json',
+        // https://developer.github.com/v3/#current-version
+    },
+};
+
 
 ghpages.publish('dist', {
     repo: `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`,
@@ -9,4 +21,16 @@ ghpages.publish('dist', {
         name: 'Github Action',
         email: 'action@github.com'
     }
-}, console.log);
+}, (err) => {
+
+    if (err) {
+        console.log(err);
+    }
+
+    fetch(BUILD_REQ_URL, opt)
+    .then(r => r.text())
+    .then(console.log)
+    ;
+});
+
+
