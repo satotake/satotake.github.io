73035f1f357f8f946989eda7d2da22a031e6a116
SatowTakeshi <doublequotation@gmail.com>
2019-10-14 17:58:01 +0900

    Rollback api posting

--- scripts/ghpages.ts
+++ scripts/ghpages.ts
@@ -2,6 +2,21 @@ import * as ghpages from 'gh-pages';
 import * as fetch from 'node-fetch';
 
 const { GITHUB_ACTOR, GITHUB_REPOSITORY, GITHUB_TOKEN, PERSONAL_TOKEN } = process.env;
+const GITHUB_API = {
+    root: 'https://api.github.com',
+    pageBuild: '/repos/satotake/satotake.github.io/pages/builds',
+};
+const BUILD_REQ_URL = GITHUB_API.root + GITHUB_API.pageBuild;
+
+const opt = {
+    method: 'POST',
+    headers: {
+        Authorization: `token ${PERSONAL_TOKEN}`,
+        Accept: 'application/vnd.github.v3+json',
+        // https://developer.github.com/v3/#current-version
+    },
+};
+
 
 ghpages.publish('dist', {
     repo: `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`,
@@ -16,6 +31,12 @@ ghpages.publish('dist', {
         console.log(err);
         return;
     }
+
+    fetch(BUILD_REQ_URL, opt)
+    .then(r => r.text())
+    .then(console.log)
+    .catch(console.error)
+    ;
 });
 
 
