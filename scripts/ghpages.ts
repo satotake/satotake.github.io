aed5d614ddd7db24c95db7f0c84b30df83a056d6
SatowTakeshi <doublequotation@gmail.com>
2019-10-14 17:05:55 +0900

    Remove explicit posting

--- scripts/ghpages.ts
+++ scripts/ghpages.ts
@@ -2,21 +2,6 @@ import * as ghpages from 'gh-pages';
 import * as fetch from 'node-fetch';
 
 const { GITHUB_ACTOR, GITHUB_REPOSITORY, GITHUB_TOKEN, PERSONAL_TOKEN } = process.env;
-const GITHUB_API = {
-    root: 'https://api.github.com',
-    pageBuild: '/repos/satotake/satotake.github.io/pages/builds',
-};
-const BUILD_REQ_URL = GITHUB_API.root + GITHUB_API.pageBuild;
-
-const opt = {
-    method: 'POST',
-    headers: {
-        Authorization: `token ${PERSONAL_TOKEN}`,
-        Accept: 'application/vnd.github.v3+json',
-        // https://developer.github.com/v3/#current-version
-    },
-};
-
 
 ghpages.publish('dist', {
     repo: `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`,
@@ -31,12 +16,6 @@ ghpages.publish('dist', {
         console.log(err);
         return;
     }
-
-    fetch(BUILD_REQ_URL, opt)
-    .then(r => r.text())
-    .then(console.log)
-    .catch(console.error)
-    ;
 });
 
 
