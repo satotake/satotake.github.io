c6c471540d37be7f1df0fa25d101c62f7959a602
SatowTakeshi <doublequotation@gmail.com>
2019-10-14 18:22:12 +0900

    refacotring

--- scripts/ghpages.ts
+++ scripts/ghpages.ts
@@ -1,3 +1,4 @@
+import {promisify} from 'util';
 import * as ghpages from 'gh-pages';
 import * as fetch from 'node-fetch';
 
@@ -8,35 +9,37 @@ const GITHUB_API = {
 };
 const BUILD_REQ_URL = GITHUB_API.root + GITHUB_API.pageBuild;
 
+const publish = promisify(ghpages.publish);
+
+const buildRequest = () => {
+    const opt = {
+        method: 'POST',
+        headers: {
+            Authorization: `token ${PERSONAL_TOKEN}`,
+            // need scope for accessing repo.
+            // e.g.) public_repo
+            Accept: 'application/vnd.github.v3+json',
+            // https://developer.github.com/v3/#current-version
+        },
+    };
+
+    return fetch(BUILD_REQ_URL, opt)
+    .then(r => {console.log(r.status); return r.text()})
+    .then(console.log)
+    .catch(console.error)
+    ;
+}
+
 const opt = {
-    method: 'POST',
-    headers: {
-        Authorization: `token ${PERSONAL_TOKEN}`,
-        Accept: 'application/vnd.github.v3+json',
-        // https://developer.github.com/v3/#current-version
-    },
-};
-
-
-ghpages.publish('dist', {
     repo: `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`,
     branch: 'master',
     user: {
         name: 'Github Action',
         email: 'action@github.com'
     }
-}, (err) => {
+};
 
-    if (err) {
-        console.log(err);
-        return;
-    }
-
-    fetch(BUILD_REQ_URL, opt)
-    .then(r => r.text())
-    .then(console.log)
-    .catch(console.error)
-    ;
-});
-
-
+publish('dist', opt)
+.then(buildRequest)
+.catch(console.error)
+;
