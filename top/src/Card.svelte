eb6189c0e10a32dd4e02872e7a2ede6f99b78411
SatowTakeshi <doublequotation@gmail.com>
2019-10-14 23:35:23 +0900

    Refactor(top) installing Tailwind

---
+++ top/src/Card.svelte
@@ -0,0 +1,59 @@
+<script>
+  const rand = parseInt(Math.random() * 10);
+  export let articleImage = 'https://picsum.photos/600/400/?random' + rand;
+  export let articleTitle = 'Article Title';
+  export let authorName = 'Anonymous';
+  export let authorImage = 'https://picsum.photos/32/32/?random' + rand;
+  export let date = 1571058774669; // first published date;
+  export let articleLink = '#';
+  export let projectStatus = 'production';
+  $: formattedDate = new Date(date).toLocaleDateString();
+
+  const projectStatusBaseClass = `
+  block rounded-full text-xs font-light px-3 py-2 leading-none flex items-center text-white
+  `;
+  const statusColor = {
+    prototypes: 'orange',
+    production: 'green',
+  };
+  $: color = statusColor[projectStatus] || 'transparent';
+  $: projectStatusClass = `${projectStatusBaseClass} bg-${color}-300`;
+</script>
+
+<article class="overflow-hidden rounded-lg shadow-lg">
+
+    <a href={articleLink}>
+        <img alt="Placeholder" class="block h-auto w-full" src={articleImage}>
+    </a>
+
+    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+        <h1 class="text-lg">
+            <a class="no-underline hover:underline text-black" href={articleLink}>
+                {articleTitle}
+            </a>
+        </h1>
+        <p class="text-grey-darker text-sm">
+            {formattedDate}
+        </p>
+    </header>
+
+    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+        <a class="flex items-center no-underline hover:underline text-black" href={articleLink}>
+            <img alt="Placeholder" class="block rounded-full" src={authorImage}>
+            <p class="ml-2 text-sm">
+                {authorName}
+            </p>
+        </a>
+
+        <a class="no-underline text-grey-darker hover:text-red-dark" href={articleLink}>
+        </a>
+
+        <div class="px+6 mt-6">
+            <span class={projectStatusClass}>
+              {projectStatus.toUpperCase()}
+            </span>
+        </div>
+    </footer>
+
+
+</article>
---
+++ top/src/Navi.svelte
@@ -0,0 +1,24 @@
+<nav class="flex items-center justify-between flex-wrap bg-teal p-6">
+  <div class="flex items-center flex-no-shrink mr-6" alt="Site Logo">
+    <img class="h-8 w-8 mr-2" width="54" height="54" src="/logo.svg" />
+    <span class="font-semibold text-xl tracking-tight">notes</span>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+      <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="https://github.com/satotake/satotake.github.io" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
+        Github
+      </a>
+      <a href="#TODO" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+        Links
+      </a>
+      <a href="#TODO" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+        About
+      </a>
+    </div>
+  </div>
+</nav>
